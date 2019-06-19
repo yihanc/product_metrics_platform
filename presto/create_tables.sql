02fdbf4bd439eb5bc2994c54cd79d95f933bf831
Sherry Shao <deeshaojob@gmail.com>
2019-06-18 23:54:14 -0700

    Update create_tables.sql

--- presto/create_tables.sql
+++ presto/create_tables.sql
@@ -31,4 +31,57 @@ CREATE TABLE dim_users (
   EXTERNAL_LOCATION ='s3://stackoverflow-ds/raw/users.parquet' 
 );
 
+CREATE TABLE dim_comments (
+  creation_date VARCHAR,
+  id VARCHAR,
+  score INT,
+  text INT,
+  user_id INT,
+  load_date VARCHAR
+) WITH ( 
+  PARTITIONED_BY = ['load_date'],
+  FORMAT = 'parquet', 
+  EXTERNAL_LOCATION ='s3://stackoverflow-ds/raw/comments.parquet' 
+);
 
+CREATE TABLE dim_posts (
+  accepted_answer_id INT,
+  answer_count INT,
+  body VARCHAR,
+  comment_count INT,
+  community_owned_date VARCHAR,
+  creation_date VARCHAR,
+  favorite_count INT,
+  id VARCHAR,
+  last_activity_date VARCHAR,
+  last_editor_display_name VARCHAR,  
+  last_editor_user_id INT,
+  last_edit_date VARCHAR,
+  owner_user_id INT,
+  score INT,
+  tags VARCHAR,
+  title VARCHAR,
+  view_count INT,
+  load_date VARCHAR,
+  post_type_id INT
+) WITH ( 
+  PARTITIONED_BY = ['load_date', 'post_type_id'],
+  FORMAT = 'parquet', 
+  EXTERNAL_LOCATION ='s3://stackoverflow-ds/raw/posts.parquet' 
+);
+
+
+CREATE TABLE fct_posthistory (
+  id INT,
+  post_id INT,
+  creation_date VARCHAR COMMENT "creation date w/ timestamp in UTC format"
+  user_id INT,
+  text VARCHAR,
+  revision_guid VARCHAR,
+  create_date VARCHAR COMMENT "Date only in YYYY-MM-DD format. Partition Only",
+  post_history_type_id INT
+) WITH ( 
+  PARTITIONED_BY = ['create_date', 'post_type_id'],
+  FORMAT = 'parquet', 
+  EXTERNAL_LOCATION ='s3://stackoverflow-ds/raw/posthistory.parquet' 
+);
