03509a1f475c40c067827ef7faed965df97ddecd
sherryshao123 <deeshaojob@gmail.com>
2019-06-20 07:26:00 +0000

    Added line chart and bar chart. Make sure they can read result from Presto DB.

--- web/app.py
+++ web/app.py
@@ -27,7 +27,6 @@ app.layout = dash_table.DataTable(
 
 
 app.layout = html.Div([
-#     html.Button('Submit', id='button'),
     html.H1(children='Hello Dash'),
 
     html.Div(children='''
@@ -38,60 +37,98 @@ app.layout = html.Div([
     dcc.Textarea(
         id="sql-state",
         placeholder='Enter a value...',
-        value='This is a TextArea component',
+        value='select * from sample limit 10',
         style={'width': '100%'}
     ),
-
     html.Button(id='submit-button', n_clicks=0, children='Submit'),
     html.Div(id='output-state'),
 
+    dcc.Dropdown(
+        id='my-dropdown',
+        options=[
+            {'label': 'Posts Per Year', 'value': 'posts_per_year'},
+#             {'label': 'Montreal', 'value': 'MTL'},
+#             {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value='NYC'
+    ),
+    # LINE CHART
+    dcc.Graph(
+        id='graph_line',
+        config={
+            'showSendToCloud': True,
+            'plotlyServerURL': 'https://plot.ly'
+        }
+    ),
+    # BAR CHART
+    dcc.Graph(
+        id='graph_bar',
+        config={
+            'showSendToCloud': True,
+            'plotlyServerURL': 'https://plot.ly'
+        }
+    ),
 ])
 
 
+# Graph Call Back
+@app.callback(
+    [dash.dependencies.Output('graph_line', 'figure'),
+    dash.dependencies.Output('graph_bar', 'figure')],
+    [dash.dependencies.Input('my-dropdown', 'value')])
+def update_bar_output(value):
 
-# @app.callback(
-#     Output(component_id='my-div', component_property='children'),
-#     [Input(component_id='my-id', component_property='value')]
-# )
-# def update_output_div(input_value):
-# 
-#     if len(input_value) < 170:
-#         return 'You\'ve entered "{}"'.format(input_value)
-#     else:
-#         payload = { "query": input_value }
-#         url = 'http://10.0.0.6:8082/druid/v2/sql'
-#         headers = {'content-type': 'application/json'}
-#         r = requests.post(url, data=json.dumps(payload), headers=headers)
-    # return 'You\'ve entered "{}". ----------------- {}. '.format(input_value, json.dumps(payload))
-#     return u'You\'ve entered "{}". ------------- Headers are {}. --------- Data is {}'.format(input_value, r.headers, r.text)
+    # sql = "select SUBSTR(creation_date,1,4) as year, count(1) as cnt from dim_posts group by 1 order by year"
+    sql = "select tag_name, cnt from dim_tags limit 10"
+    rows = exec_presto(sql)
+
+    x_values = [ row[0] for row in rows ]
+    y_values = [ row[1] for row in rows ]
+    result_line = {
+        'data': [{
+            'type': 'scatter',
+            'x': x_values,
+            'y': y_values,
+        }],
+        'layout': {
+            'title': value
+        }
+    }
+
+    result_bar = {
+        'data': [
+            go.Bar(
+                x=x_values,
+                y=y_values,
+                name='Bar',
+                marker=go.bar.Marker(
+                    color='rgb(55, 83, 109)'
+                )
+            ),
+        ],
+        'layout': go.Layout(
+            title='Bar Chart',
+            showlegend=True,
+            legend=go.layout.Legend(
+                x=0,
+                y=1.0
+            ),
+            margin=go.layout.Margin(l=40, r=0, t=40, b=30)
+        ),
+    }
+    return result_line, result_bar
 
 
-
+# Text Input Callback
 @app.callback(Output('output-state', 'children'),
               [Input('submit-button', 'n_clicks')],
               [State('sql-state', 'value'),])
-#              [State('input-1-state', 'value'),])
-#                State('input-2-state', 'value')])
 def update_output(n_clicks, input1):
+    # Remove semicolons if any
+    input1 = input1.replace(';', '') 
 
-    import prestodb
-    conn=prestodb.dbapi.connect(
-        host='localhost',
-        port=8889,
-        user='hadoop',
-        catalog='hive',
-        schema='web',
-    )
-    cur = conn.cursor()
-    cur.execute(input1)
-    rows = cur.fetchall()
-   # cmd = "/usr/bin/presto-cli"
-
-    #cp = subprocess.run([cmd, "--catalog", "hive", "--schema", "web", "--execute", input1], stdout=subprocess.PIPE)
-
+    rows = exec_presto(input1)
     print(rows)
-    #print(cp.stdout)
-
 
     return u'''
         The Button has been pressed {} times,
@@ -101,7 +138,18 @@ def update_output(n_clicks, input1):
     '''.format(n_clicks, input1, rows)
 
 
-
-
+def exec_presto(sql):
+    import prestodb
+    conn=prestodb.dbapi.connect(
+        host='localhost',
+        port=8889,
+        user='hadoop',
+        catalog='hive',
+        schema='web',
+    )
+    cur = conn.cursor()
+    cur.execute(sql)
+    return cur.fetchall()
+    
 if __name__ == '__main__':
     app.run_server(debug=True, host='0.0.0.0')
