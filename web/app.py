bf1ef43c2ecb49050ba5648c20ec60d0b6d17691
sherryshao123 <deeshaojob@gmail.com>
2019-06-20 22:25:31 -0700

    t

--- web/app.py
+++ web/app.py
@@ -11,7 +11,7 @@ import pandas as pd
 import requests, json
 import subprocess
 import prestodb
-
+# ttt
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
