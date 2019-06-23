c03fd6255820bc5af0e0c2d3e5dfa70bc4415fb2
sherryshao123 <deeshaojob@gmail.com>
2019-06-23 22:56:40 +0000

    modified producer

--- kafka/producer.py
+++ kafka/producer.py
@@ -1,6 +1,28 @@
-import boto3
+#!/usr/bin/python3
+from smart_open import open
+import time
+import lxml.etree
 
-s3 = boto3.resource('s3')
-obj = s3.Object('stackoverflow-ds', 'PostHistory.xml')
-body = obj.get()['Body'].read()
-print(body)
+for i, line in enumerate(open('s3://stackoverflow-ds/PostHistory_rt.xml')):
+    if i <= 1:  # Skip first two lines
+        continue
+    print(" ")
+    print(line)
+    
+
+    schema = ["Comment", 'Id', 'PostHistoryTypeId', 'PostId', 'RevisionGUID', 'Text', 'UserDisplayName', 'UserId']
+    parsed = {}
+
+    for col in schema:
+        value = lxml.etree.fromstring(line).xpath('//row/@{}'.format(col))
+        parsed[col] = value[0] if len(value) > 0 else ""
+
+    print(parsed)
+
+    # Load into Kafka server
+
+    if i % 5 == 0:
+        print("resting 5 sec...")
+        time.sleep(5)
+    if i == 1000:
+        break
