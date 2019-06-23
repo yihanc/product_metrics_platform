2983c1ef909bf2cdf3670896e225d55b8f45963f
sherryshao123 <deeshaojob@gmail.com>
2019-06-23 14:51:33 -0700

    add kafka producer.py

--- kafka/producer.py
+++ kafka/producer.py
@@ -1,9 +1,6 @@
 import boto3
 
-client = boto3.client('s3') #low-level functional API
-
-
-obj = client.get_object(Bucket='stackoverflow-ds', Key='PostHistory.xml')
-
-print(obj)
-grid_sizes = pd.read_csv(obj['Body'])
+s3 = boto3.resource('s3')
+obj = s3.Object('stackoverflow-ds', 'PostHistory.xml')
+body = obj.get()['Body'].read()
+print(body)
