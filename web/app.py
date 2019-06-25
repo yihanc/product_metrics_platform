4954d319a0f7c762c46e5b6552ab2e8c82d7373d
sherryshao123 <deeshaojob@gmail.com>
2019-06-25 09:02:27 +0000

    Added nav bar and style.

--- web/app.py
+++ web/app.py
@@ -4,50 +4,83 @@ import dash
 import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 import plotly.graph_objs as go
 import requests, json, subprocess, prestodb, time
 from kafka import KafkaConsumer, TopicPartition
 
-################################################################################
-###
-### HTML CSS
-###
-################################################################################
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+################################################################################
+###
+### Nav Bar
+###
+################################################################################
+
+PLOTLY_LOGO = "https://images.plot.ly/logo/new-branding/plotly-logomark.png"
+
+nav_item = dbc.NavItem(dbc.NavLink("Link", href="#"))
+navbar = dbc.Navbar(
+    dbc.Container(
+        [
+            html.A(
+                # Use row and col to control vertical alignment of logo / brand
+                dbc.Row(
+                    [
+                        dbc.Col(html.Img(src=PLOTLY_LOGO, height="50px")),
+                        dbc.Col(dbc.NavbarBrand("Product Metrics Platform", className="ml-2")),
+                    ],
+                    align="true",
+                    no_gutters=True,
+                ),
+                href="https://plot.ly",
+            ),
+            dbc.NavbarToggler(id="navbar-toggler2"),
+            dbc.Collapse(
+                dbc.Nav(
+                    [nav_item], className="ml-auto", navbar=True
+                ),
+                id="navbar-collapse2",
+                navbar=True,
+            ),
+        ]
+    ),
+    color="dark",
+    dark=True,
+    className="mb-5",
+)
+################################################################################
+###
+### APP Start
+###
+################################################################################
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+# app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+# app = dash.Dash(external_stylesheets=[dbc.themes.JOURNAL])
+app = dash.Dash(external_stylesheets=[dbc.themes.SKETCHY])
+
+# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
-    html.H1(children='Product Metrics Platform'),
+    navbar,
+    # html.H1(children='Product Metrics Platform'),
 
     html.Div(children='''
         Product Metrics Platform: A web application to discover and explore predefined metrics fast and easily.
     '''),
 
-
-    dcc.Textarea(
-        id="sql-state",
-        placeholder='Enter a SQL query... eg: select * from sample limit 10',
-        style={'width': '60%'}
-    ),
-    html.Button(id='submit-button', n_clicks=0, children='Submit'),
-    html.H3(children='Presto Result:'),
-    html.Div(id='presto-state'),
-    html.P(),
-    html.H3(children='Druid Result: '),
-    html.Div(id='druid-state'),
-    html.P(),
-
+    ###########################################################################
+    html.H3(children='Select a predefined metric and see result: '),
     dcc.Dropdown(
         id='my-dropdown',
         options=[
             {
-                'label': 'Top 10 Tags', 
+                'label': 'Top 10 Tags',
                 'value': 'select tag_name, cnt from dim_tags limit 10',
             },
             {
-                'label': 'Posts Per Year (Slow About  ~1 min)', 
+                'label': 'Posts Per Year (Slow About  ~1 min)',
                 'value': "select SUBSTR(creation_date,1,4) as year, count(1) as cnt from dim_posts group by 1 order by year"
             },
         ],
@@ -70,6 +103,25 @@ app.layout = html.Div([
             'plotlyServerURL': 'https://plot.ly'
         }
     ),
+    ###########################################################################
+    html.H3(children='Compare Presto and Druid Speed:'),
+    html.Div(children='''
+        Try type "select count(1) from dim_posts" and see the differences..
+    '''),
+    dcc.Textarea(
+        id="sql-state",
+        placeholder='Enter a SQL query... eg: select * from sample limit 10',
+        style={'width': '60%'}
+    ),
+    html.Button(id='submit-button', n_clicks=0, children='Submit'),
+    html.H3(children='Presto Result:'),
+    html.Div(id='presto-state'),
+    html.P(),
+    html.H3(children='Druid Result: '),
+    html.Div(id='druid-state'),
+    html.P(),
+
+    ###########################################################################
 ])
 
 
