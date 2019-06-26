30a2b1e2bae4859ae65884e0e24522b056eab28d
sherryshao123 <deeshaojob@gmail.com>
2019-06-26 06:23:08 +0000

    Combined 3 pages into tabs in app.py. Removed app_live

--- web/app.py
+++ web/app.py
@@ -7,7 +7,7 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 import plotly.graph_objs as go
-import requests, json, subprocess, prestodb, time
+import requests, json, subprocess, prestodb, time, datetime
 from kafka import KafkaConsumer, TopicPartition
 
 
@@ -70,58 +70,85 @@ app.layout = html.Div([
         Product Metrics Platform: A web application to discover and explore predefined metrics fast and easily.
     '''),
 
-    ###########################################################################
-    html.H3(children='Select a predefined metric and see result: '),
-    dcc.Dropdown(
-        id='my-dropdown',
-        options=[
-            {
-                'label': 'Top 10 Tags',
-                'value': 'select tag_name, cnt from dim_tags limit 10',
-            },
-            {
-                'label': 'Posts Per Year (Slow About  ~1 min)',
-                'value': "select SUBSTR(creation_date,1,4) as year, count(1) as cnt from dim_posts group by 1 order by year"
-            },
-        ],
-        placeholder="Select a metric",
-        value='',
-    ),
-    # LINE CHART
-    dcc.Graph(
-        id='graph_line',
-        config={
-            'showSendToCloud': True,
-            'plotlyServerURL': 'https://plot.ly'
-        }
-    ),
-    # BAR CHART
-    dcc.Graph(
-        id='graph_bar',
-        config={
-            'showSendToCloud': True,
-            'plotlyServerURL': 'https://plot.ly'
-        }
-    ),
-    ###########################################################################
-    html.H3(children='Compare Presto and Druid Speed:'),
-    html.Div(children='''
-        Try type "select count(1) from dim_posts" and see the differences..
-    '''),
-    dcc.Textarea(
-        id="sql-state",
-        placeholder='Enter a SQL query... eg: select * from sample limit 10',
-        style={'width': '60%'}
-    ),
-    html.Button(id='submit-button', n_clicks=0, children='Submit'),
-    html.H3(children='Presto Result:'),
-    html.Div(id='presto-state'),
-    html.P(),
-    html.H3(children='Druid Result: '),
-    html.Div(id='druid-state'),
-    html.P(),
-
-    ###########################################################################
+    dcc.Tabs(id="tabs", children=[
+        dcc.Tab(label='Tab one', children=[
+            html.H3(children='Select a predefined metric and see result: '),
+            dcc.Dropdown(
+                id='my-dropdown',
+                options=[
+                    {
+                        'label': 'Top 10 Tags',
+                        'value': 'select tag_name, cnt from dim_tags limit 10',
+                    },
+                    {
+                    'label': 'Posts Per Year (Slow About  ~1 min)',
+                    'value': "select SUBSTR(creation_date,1,4) as year, count(1) as cnt from dim_posts group by 1 order by year"
+                    },
+                ],
+                placeholder="Select a metric",
+                value='',
+            ),
+            # LINE CHART
+            dcc.Graph(
+                id='graph_line',
+                config={
+                    'showSendToCloud': True,
+                'plotlyServerURL': 'https://plot.ly'
+                }
+            ),
+            # BAR CHART
+            dcc.Graph(
+                id='graph_bar',
+                config={
+                    'showSendToCloud': True,
+                    'plotlyServerURL': 'https://plot.ly'
+                }
+            ),
+        ]),
+        dcc.Tab(label='Adhoc Query - (Compare Druid and Presto)', children=[
+            html.H3(children='Compare Presto and Druid Speed:'),
+            html.Div(children='''
+                Try type "select count(1) from dim_posts" and see the differences..
+            '''),
+            dcc.Textarea(
+                id="sql-state",
+                placeholder='Enter a SQL query... eg: select * from sample limit 10',
+                style={'width': '60%'}
+            ),
+            html.Button(id='submit-button', n_clicks=0, children='Submit'),
+            html.H3(children='Presto Result:'),
+            html.Div(id='presto-state'),
+            html.P(),
+            html.H3(children='Druid Result: '),
+            html.Div(id='druid-state'),
+            html.P(),
+        ]),
+        dcc.Tab(label='Live Data', children=[
+            html.H4('TERRA Satellite Live Feed'),
+            html.Div(id='live_text'),
+            # LINE CHART
+            dcc.Graph(
+                id='live_graph_line',
+                config={
+                    'showSendToCloud': True,
+                    'plotlyServerURL': 'https://plot.ly'
+                }
+            ),
+            # BAR CHART
+            dcc.Graph(
+                id='live_graph_bar',
+                config={
+                    'showSendToCloud': True,
+                    'plotlyServerURL': 'https://plot.ly'
+                }
+            ),
+            dcc.Interval(
+                id='live_interval_component',
+                interval=5*1000, # in milliseconds
+                n_intervals=0
+            )            
+        ]),
+    ]),
 ])
 
 
@@ -217,6 +244,83 @@ def get_druid_state(n_clicks, sql):
     '''.format(n_clicks, sql, dur, druid_result)
 
 
+@app.callback(Output('live_text', 'children'),
+              [Input('live_interval_component', 'n_intervals')])
+def update_metrics(n):
+    # Latest
+    dau = kafka_load_dau(seconds=1800)
+    dau_str = json.dumps(dau)
+
+    style = {'padding': '5px', 'fontSize': '30px'}
+
+    return [ html.Span('Message: {}'.format(json.dumps(dau)), style=style)]
+
+
+# Multiple components can update everytime interval gets fired.
+@app.callback(
+    [Output('live_graph_line', 'figure'),
+    Output('live_graph_bar', 'figure')],
+    [Input('live_interval_component', 'n_intervals')])
+def update_graph_live(n):
+    # Latest
+    dau = kafka_load_dau(seconds=1800)
+    dau_str = json.dumps(dau)
+
+    datetime.datetime.now().strftime('%Y-%m-%dT%H-%M')
+    minutes = 30 # minutes
+
+    data = {
+        'time': [],
+        'dau': [],
+    }
+
+    # Collect data
+    for i in range(minutes):
+        now = datetime.datetime.now()
+        before = now - datetime.timedelta(seconds=i*60)
+
+        key = before.strftime('%Y-%m-%dT%H-%M')
+        value = dau[key] if key in dau else 0
+
+        data['time'].append(before)
+        data['dau'].append(value)
+        
+    result_line = {
+        'data': [{
+            'type': 'scatter',
+            'x': data['time'],
+            'y': data['dau'],
+        }],
+        'layout': {
+            'title': value
+        }
+    }
+
+    result_bar = {
+        'data': [
+            go.Bar(
+                x=data['time'],
+                y=data['dau'],
+                name='Bar',
+                marker=go.bar.Marker(
+                    color='rgb(55, 83, 109)'
+                )
+            ),
+        ],
+        'layout': go.Layout(
+            title='Bar Chart',
+            showlegend=True,
+            legend=go.layout.Legend(
+                x=0,
+                y=1.0
+            ),
+            margin=go.layout.Margin(l=40, r=0, t=40, b=30)
+        ),
+    }
+
+    return result_line, result_bar
+
+
 
 ################################################################################
 ###
@@ -254,7 +358,33 @@ def exec_druid(sql):
     r = requests.post(url=druid_url, data=msg, headers=headers)
     # print("r: ", r, r.text)
     return r.text, time.clock() - start
-    
+ 
+# Load live data from Kafka
+def kafka_load_dau(seconds=1800):
+    dau = {}
+
+    consumer = KafkaConsumer(
+        'posthistory', 
+        bootstrap_servers=['172.31.7.229:9092'], 
+        auto_offset_reset='earliest', 
+        enable_auto_commit=True, 
+        auto_commit_interval_ms=1000
+    )
+
+    # Finding end offset so that we could stop the loop.
+    next(consumer)
+    partition = consumer.assignment().pop()
+    end_offset = consumer.end_offsets([partition])[partition] - 1
+
+    for raw_msg in consumer:
+        msg = raw_msg.value.decode('utf-8')
+        msg_data = json.loads(msg)
+        ts = msg_data['_CreationDate'][:16]
+        dau[ts] = dau.get(ts, 0) + 1
+        if raw_msg.offset == end_offset:
+            break
+    return dau
+
     
 
 ################################################################################
